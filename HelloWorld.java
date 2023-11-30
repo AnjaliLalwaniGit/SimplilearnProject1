85ac6cfc48ac895f2be7ad72654f3e9f3b0f118c
Anjali_Lalwani <111319739+AnjaliLalwaniGit@users.noreply.github.com>
2023-11-29 16:45:04 -0800

    Update HelloWorld.java
    Made changes to the console output

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-      System.out.println("Java app created for Project1-Simplilearn");
+      System.out.println("Java app created for Project1-Simplilearn--this is the edited version");
     }
   }
